24107042056c6f2d4d48ccef20c50f08b30039a2
Ferdinand <ndimbecha@gmail.com>
2021-03-31 03:13:50 +0200

    Do arithmetic2 numbers and booleans

---
+++ arithmetic2.js
@@ -0,0 +1,45 @@
+//1. Declare a variable and assign a string as its value.Make sure the string is at least 6 characters long.Print the variable to the console.
+const bullets = "lovingly";
+
+//2. Access the 2nd and 6th elements of the string using index notation. Print the elements to the console.
+console.log(bullets[1]);
+console.log(bullets[5]);
+
+//3. Declare a variable called `name` and assign a string as its value.
+let name = "Agbor";
+
+//4. Declare a variable called `age` and assign a number as its value.
+let age = 81;
+//5. Declare a variable called `year` and assign a number as its value.
+let year = 2035;
+
+//6. Using your declared variables, print a string to the console. For example, "Basem is 37 in 2020".
+console.log(` ${name} is ${age} years old. He plans to live until  ${year}`);
+
+//7. Declare a variable called `isMarried` and assign it a boolean value.
+
+const isMarried = true;
+
+//8. Print a string to the console, indicating whether or not your person is married. For example, "Basem is married: true"
+console.log(`${name} is married: ${isMarried}`);
+
+//9. Declare a variable called `programmingLanguage` and assign "JavaScript" as its value. Declare a variable called `isFun` and give it a value of `true`.  Print "JavaScript is fun: true" to the console, using your declared variables.
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(`${programmingLanguage} is fun: ${isFun}`);
+
+//10. Print the `J` and `S` of "JavaScript" from the variable `programmingLanguage` to the console.
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[4]);
+
+//11. Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[1]);
+console.log(programmingLanguage[2]);
+console.log(programmingLanguage[3]);
+console.log(programmingLanguage[4]);
+console.log(programmingLanguage[5]);
+console.log(programmingLanguage[6]);
+console.log(programmingLanguage[7]);
+console.log(programmingLanguage[8]);
+console.log(programmingLanguage[9]);
